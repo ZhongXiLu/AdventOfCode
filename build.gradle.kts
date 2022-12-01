7e43f6f27b8b764957dcbf53125da13df26abbc8
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-01 19:15:14 +0100

    Fix slf4j warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.reflections", "reflections", "0.10.2")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
