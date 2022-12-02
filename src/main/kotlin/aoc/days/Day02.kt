e92783db8dae1b6de99f268488edf1b954f339ba
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-02 18:54:06 +0100

    Day 2, refactoring

--- src/main/kotlin/aoc/days/Day02.kt
+++ src/main/kotlin/aoc/days/Day02.kt
@@ -8,58 +8,51 @@ private const val SCISSOR_OPP = "C"
 private const val ROCK_SELF = "X"
 private const val PAPER_SELF = "Y"
 private const val SCISSOR_SELF = "Z"
+private const val LOSE_SCORE = 0
+private const val DRAW_SCORE = 3
+private const val WIN_SCORE = 6
 private const val LOSE = "X"
 private const val DRAW = "Y"
 private const val WIN = "Z"
+
+private val SCORE_OUTCOMES = hashMapOf<String, Map<String, Int>>(
+    ROCK_OPP to hashMapOf(SCISSOR_SELF to LOSE_SCORE, ROCK_SELF to DRAW_SCORE, PAPER_SELF to WIN_SCORE),
+    PAPER_OPP to hashMapOf(ROCK_SELF to LOSE_SCORE, PAPER_SELF to DRAW_SCORE, SCISSOR_SELF to WIN_SCORE),
+    SCISSOR_OPP to hashMapOf(PAPER_SELF to LOSE_SCORE, SCISSOR_SELF to DRAW_SCORE, ROCK_SELF to WIN_SCORE)
+)
+
+private val SHAPE_SELECTION = hashMapOf<String, Map<String, String>>(
+    ROCK_OPP to hashMapOf(LOSE to SCISSOR_SELF, DRAW to ROCK_SELF, WIN to PAPER_SELF),
+    PAPER_OPP to hashMapOf(LOSE to ROCK_SELF, DRAW to PAPER_SELF, WIN to SCISSOR_SELF),
+    SCISSOR_OPP to hashMapOf(LOSE to PAPER_SELF, DRAW to SCISSOR_SELF, WIN to ROCK_SELF)
+)
 
 class Day02 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
         return input
             .map { it.split(" ") }
-            .map { calculateScoreOutcomeOfRound(it[0], it[1]) + calculateScoreShapeSelected(it[1]) }
+            .map { (opponentShape, ownShape) ->
+                calculateScoreOutcome(opponentShape, ownShape) + calculateScoreShape(ownShape)
+            }
             .sum()
     }
 
     override fun solvePart2(input: List<String>): Any {
         return input
             .map { it.split(" ") }
-            .map { listOf(it[0], chooseOwnShape(it[0], it[1])) }
-            .map { calculateScoreOutcomeOfRound(it[0], it[1]) + calculateScoreShapeSelected(it[1]) }
+            .map { (opponentShape, ownShape) -> listOf(opponentShape, chooseOwnShape(opponentShape, ownShape)) }
+            .map { (opponentShape, ownShape) ->
+                calculateScoreOutcome(opponentShape, ownShape) + calculateScoreShape(ownShape)
+            }
             .sum()
     }
 
-    private fun calculateScoreOutcomeOfRound(opponentShape: String, ownShape: String): Int {
-        return when (opponentShape) {
-            ROCK_OPP -> {
-                when (ownShape) {
-                    ROCK_SELF -> 3
-                    PAPER_SELF -> 6
-                    SCISSOR_SELF -> 0
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            PAPER_OPP -> {
-                when (ownShape) {
-                    ROCK_SELF -> 0
-                    PAPER_SELF -> 3
-                    SCISSOR_SELF -> 6
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            SCISSOR_OPP -> {
-                when (ownShape) {
-                    ROCK_SELF -> 6
-                    PAPER_SELF -> 0
-                    SCISSOR_SELF -> 3
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            else -> throw Exception("Unknown shape: $opponentShape")
-        }
+    private fun calculateScoreOutcome(opponentShape: String, ownShape: String): Int {
+        return SCORE_OUTCOMES[opponentShape]!![ownShape]!!
     }
 
-    private fun calculateScoreShapeSelected(shape: String): Int {
+    private fun calculateScoreShape(shape: String): Int {
         return when (shape) {
             ROCK_SELF -> 1
             PAPER_SELF -> 2
@@ -69,33 +62,7 @@ class Day02 : Day() {
     }
 
     private fun chooseOwnShape(opponentShape: String, ownShape: String): String {
-        return when (opponentShape) {
-            ROCK_OPP -> {
-                when (ownShape) {
-                    LOSE -> SCISSOR_SELF
-                    DRAW -> ROCK_SELF
-                    WIN -> PAPER_SELF
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            PAPER_OPP -> {
-                when (ownShape) {
-                    LOSE -> ROCK_SELF
-                    DRAW -> PAPER_SELF
-                    WIN -> SCISSOR_SELF
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            SCISSOR_OPP -> {
-                when (ownShape) {
-                    LOSE -> PAPER_SELF
-                    DRAW -> SCISSOR_SELF
-                    WIN -> ROCK_SELF
-                    else -> throw Exception("Unknown shape: $ownShape")
-                }
-            }
-            else -> throw Exception("Unknown shape: $opponentShape")
-        }
+        return SHAPE_SELECTION[opponentShape]!![ownShape]!!
     }
 
 }
