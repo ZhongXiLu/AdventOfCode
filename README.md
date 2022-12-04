18098f3009acea77b7c0c60b8c5e221a008d58e6
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-04 10:53:26 +0100

    Day 4

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 | 01  | [Calorie Counting](https://adventofcode.com/2022/day/1)        |   🌟   |   🌟   | [![Code](https://img.shields.io/badge/code-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white)](src/main/kotlin/aoc/days/Day01.kt) |
 | 02  | [Rock Paper Scissors](https://adventofcode.com/2022/day/2)     |   🌟   |   🌟   | [![Code](https://img.shields.io/badge/code-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white)](src/main/kotlin/aoc/days/Day02.kt) |
 | 03  | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) |   🌟   |   🌟   | [![Code](https://img.shields.io/badge/code-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white)](src/main/kotlin/aoc/days/Day03.kt) |
+| 04  | [Camp Cleanup](https://adventofcode.com/2022/day/4)            |   🌟   |   🌟   | [![Code](https://img.shields.io/badge/code-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white)](src/main/kotlin/aoc/days/Day04.kt) |
 
---
+++ src/main/kotlin/aoc/days/Day04.kt
@@ -0,0 +1,33 @@
+package aoc.days
+
+import aoc.Day
+
+class Day04 : Day() {
+
+    override fun solvePart1(input: List<String>): Any {
+        return input
+            .asSequence()
+            .mapToRanges()
+            .map { (first, second) -> rangesOverlap(first, second) }
+            .count { it }
+    }
+
+    override fun solvePart2(input: List<String>): Any {
+        return input
+            .asSequence()
+            .mapToRanges()
+            .map { (first, second) -> first.intersect(second).isNotEmpty() }
+            .count { it }
+    }
+
+    private fun rangesOverlap(firstRange: IntRange, secondRange: IntRange): Boolean {
+        return (firstRange.contains(secondRange.first) && firstRange.contains(secondRange.last)) ||
+                (secondRange.contains(firstRange.first) && secondRange.contains(firstRange.last))
+    }
+
+    private fun Sequence<String>.mapToRanges(): Sequence<Pair<IntRange, IntRange>> =
+        map { it.split(",") }
+            .map { (first, second) -> Pair(first.split("-"), second.split("-")) }
+            .map { (first, second) -> Pair(first[0].toInt()..first[1].toInt(), second[0].toInt()..second[1].toInt()) }
+
+}
