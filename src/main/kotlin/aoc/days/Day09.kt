c3112f7b5cb3edf1e73ebfb5c87fab90bc79a926
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-09 20:56:37 +0100

    Day 9, part 2

--- src/main/kotlin/aoc/days/Day09.kt
+++ src/main/kotlin/aoc/days/Day09.kt
@@ -12,25 +12,37 @@ class Day09 : Day() {
             .map { it.split(" ") }
             .forEach { (direction, steps) -> bridge.moveHead(direction, steps.toInt()) }
 
-        return bridge.tailPositions.size
+        return bridge.tailEndPositions.size
     }
 
     override fun solvePart2(input: List<String>): Any {
-        return ""
+        val bridge = Bridge()
+        repeat(8) { bridge.tails.add(Pair(0, 0)) }
+
+        input
+            .map { it.split(" ") }
+            .forEach { (direction, steps) -> bridge.moveHead(direction, steps.toInt()) }
+
+        return bridge.tailEndPositions.size
     }
 
 }
 
 private class Bridge {
     var head = Pair(0, 0)
-    var tail = Pair(0, 0)
-    var tailPositions = mutableSetOf(tail)
+    var tails = mutableListOf(Pair(0, 0))
+    var tailEndPositions = mutableSetOf(tails.last())
 
     fun moveHead(direction: String, steps: Int) {
         repeat(steps) {
             moveHeadOneStep(direction)
-            adjustTail()
-            tailPositions.add(tail)
+            tails[0] = adjustTailTo(tails[0], head)
+
+            // Move other tail parts
+            (1 until tails.size)
+                .forEach { tails[it] = adjustTailTo(tails[it], tails[it - 1]) }
+
+            tailEndPositions.add(tails.last())
         }
     }
 
@@ -44,27 +56,28 @@ private class Bridge {
         }
     }
 
-    private fun adjustTail() {
-        if (tailNotTouchingHead()) {
-            if (tail.first == head.first) {
+    private fun adjustTailTo(tail: Pair<Int, Int>, head: Pair<Int, Int>): Pair<Int, Int> {
+        if (tailNotTouchingHead(tail, head)) {
+            return if (tail.first == head.first) {
                 // Same row
-                tail = Pair(tail.first, tail.second + if (tail.second < head.second) 1 else -1)
+                Pair(tail.first, tail.second + if (tail.second < head.second) 1 else -1)
 
             } else if (tail.second == head.second) {
                 // Same column
-                tail = Pair(tail.first + if (tail.first < head.first) 1 else -1, tail.second)
+                Pair(tail.first + if (tail.first < head.first) 1 else -1, tail.second)
 
             } else {
                 // Move diagonally
-                tail = Pair(
+                Pair(
                     tail.first + if (tail.first < head.first) 1 else -1,
                     tail.second + if (tail.second < head.second) 1 else -1
                 )
             }
         }
+        return tail
     }
 
-    private fun tailNotTouchingHead(): Boolean {
+    private fun tailNotTouchingHead(tail: Pair<Int, Int>, head: Pair<Int, Int>): Boolean {
         return abs(tail.first.minus(head.first)) > 1 || abs(tail.second.minus(head.second)) > 1
     }
 }
