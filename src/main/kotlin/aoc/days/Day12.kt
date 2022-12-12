91c7b229aed26fb40e9e77fa0326f360d9956878
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-12 17:55:23 +0100

    Day 12, refactoring (could be better...)

--- src/main/kotlin/aoc/days/Day12.kt
+++ src/main/kotlin/aoc/days/Day12.kt
@@ -5,139 +5,97 @@ import aoc.Day
 class Day12 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
-        val hill = input
-            .map { it.toCharArray() }
-        val startingPosition = findStartingPositions(hill, 'S').first()
+        val hill = input.map { it.toCharArray() }
 
-        return stepsToEnd(hill, startingPosition)
+        return hill.findPosition('S')
+            .map { startingPosition -> hill.stepsToEnd(startingPosition) }
+            .first()
     }
 
     override fun solvePart2(input: List<String>): Any {
-        val hill = input
-            .map { it.toCharArray() }
-        return findStartingPositions(hill, 'a')
-            .minOf { startingPosition -> stepsToEnd(hill, startingPosition) }
+        val hill = input.map { it.toCharArray() }
+
+        return hill.findPosition('a')
+            .minOf { startingPosition -> hill.stepsToEnd(startingPosition) }
     }
 
-    private fun findStartingPositions(hill: List<CharArray>, startingPositionSymbol: Char): List<Pair<Int, Int>> {
-        return hill
-            .indices
-            .map { row ->
-                hill.first().indices
-                    .filter { column -> hill[row][column] == startingPositionSymbol }
-                    .map { column -> Pair(row, column) }
-            }
-            .flatten()
-    }
+}
 
-    private fun stepsToEnd(hill: List<CharArray>, startingPosition: Pair<Int, Int>): Int {
-        val visited = hill.indices
-            .map {
-                hill.first().indices
-                    .map { Pair(false, 99999) }
-                    .toMutableList()
-            }
+private fun List<CharArray>.findPosition(startingPositionSymbol: Char): List<Pair<Int, Int>> {
+    return this
+        .indices
+        .map { row ->
+            this.first().indices
+                .filter { column -> this[row][column] == startingPositionSymbol }
+                .map { column -> Pair(row, column) }
+        }
+        .flatten()
+}
 
-        val nextSquares = mutableListOf(startingPosition)
-        visited[startingPosition.first][startingPosition.second] = Pair(false, 0)
-
-        while (nextSquares.isNotEmpty()) {
-            val currentSquarePos = nextSquares.removeFirst()
-
-            val (currentSquareVisited, currentSquareDistance) = visited[currentSquarePos.first][currentSquarePos.second]
-            if (!currentSquareVisited) {
-                visited[currentSquarePos.first][currentSquarePos.second] = Pair(true, currentSquareDistance)
-
-                if (currentSquarePos.first - 1 >= 0) {
-                    if (atMostOneStepHigher(
-                            hill,
-                            currentSquarePos,
-                            Pair(currentSquarePos.first - 1, currentSquarePos.second)
-                        )
-                    ) {
-                        nextSquares.add(Pair(currentSquarePos.first - 1, currentSquarePos.second))
-                        val distanceToNextSquare = visited[currentSquarePos.first - 1][currentSquarePos.second].second
-                        if (currentSquareDistance + 1 < distanceToNextSquare) {
-                            visited[currentSquarePos.first - 1][currentSquarePos.second] = Pair(
-                                visited[currentSquarePos.first - 1][currentSquarePos.second].first,
-                                currentSquareDistance + 1
-                            )
-                        }
-                    }
-                }
-                if (currentSquarePos.first + 1 < hill.size) {
-                    if (atMostOneStepHigher(
-                            hill,
-                            currentSquarePos,
-                            Pair(currentSquarePos.first + 1, currentSquarePos.second)
-                        )
-                    ) {
-                        nextSquares.add(Pair(currentSquarePos.first + 1, currentSquarePos.second))
-                        val distanceToNextSquare = visited[currentSquarePos.first + 1][currentSquarePos.second].second
-                        if (currentSquareDistance + 1 < distanceToNextSquare) {
-                            visited[currentSquarePos.first + 1][currentSquarePos.second] = Pair(
-                                visited[currentSquarePos.first + 1][currentSquarePos.second].first,
-                                currentSquareDistance + 1
-                            )
-                        }
-                    }
-                }
-                if (currentSquarePos.second - 1 >= 0) {
-                    if (atMostOneStepHigher(
-                            hill,
-                            currentSquarePos,
-                            Pair(currentSquarePos.first, currentSquarePos.second - 1)
-                        )
-                    ) {
-                        nextSquares.add(Pair(currentSquarePos.first, currentSquarePos.second - 1))
-                        val distanceToNextSquare = visited[currentSquarePos.first][currentSquarePos.second - 1].second
-                        if (currentSquareDistance + 1 < distanceToNextSquare) {
-                            visited[currentSquarePos.first][currentSquarePos.second - 1] = Pair(
-                                visited[currentSquarePos.first][currentSquarePos.second - 1].first,
-                                currentSquareDistance + 1
-                            )
-                        }
-                    }
-                }
-                if (currentSquarePos.second + 1 < hill.first().size) {
-                    if (atMostOneStepHigher(
-                            hill,
-                            currentSquarePos,
-                            Pair(currentSquarePos.first, currentSquarePos.second + 1)
-                        )
-                    ) {
-                        nextSquares.add(Pair(currentSquarePos.first, currentSquarePos.second + 1))
-                        val distanceToNextSquare = visited[currentSquarePos.first][currentSquarePos.second + 1].second
-                        if (currentSquareDistance + 1 < distanceToNextSquare) {
-                            visited[currentSquarePos.first][currentSquarePos.second + 1] = Pair(
-                                visited[currentSquarePos.first][currentSquarePos.second + 1].first,
-                                currentSquareDistance + 1
-                            )
-                        }
-                    }
-                }
-            }
+private fun List<CharArray>.stepsToEnd(startingPosition: Pair<Int, Int>): Int {
+    val visited = this.indices
+        .map {
+            this.first().indices
+                .map { Pair(false, Integer.MAX_VALUE) }
+                .toMutableList()
         }
 
-        var endPosition = Pair(0, 0)
-        hill
-            .indices
-            .forEach { row ->
-                hill.first().indices
-                    .forEach { column -> if (hill[row][column] == 'E') endPosition = Pair(row, column) }
+    val nextSquares = mutableListOf(startingPosition)
+    visited[startingPosition.first][startingPosition.second] = Pair(false, 0)
+
+    while (nextSquares.isNotEmpty()) {
+        val currentSquarePos = nextSquares.removeFirst()
+
+        val (currentSquareVisited, currentSquareDistance) = visited[currentSquarePos.first][currentSquarePos.second]
+        if (!currentSquareVisited) {
+            visited[currentSquarePos.first][currentSquarePos.second] = Pair(true, currentSquareDistance)
+
+            if (currentSquarePos.first - 1 >= 0) {
+                val nextSquare = Pair(currentSquarePos.first - 1, currentSquarePos.second)
+                visitSquare(currentSquarePos, currentSquareDistance, nextSquare, nextSquares, visited)
             }
-
-        return visited[endPosition.first][endPosition.second].second
-    }
-
-    private fun atMostOneStepHigher(hill: List<CharArray>, current: Pair<Int, Int>, next: Pair<Int, Int>): Boolean {
-        return if (hill[current.first][current.second] == 'S') {
-            hill[next.first][next.second] == 'a'
-        } else if (hill[next.first][next.second] == 'E') {
-            true
-        } else {
-            hill[next.first][next.second].minus(hill[current.first][current.second]) <= 1
+            if (currentSquarePos.first + 1 < this.size) {
+                val nextSquare = Pair(currentSquarePos.first + 1, currentSquarePos.second)
+                visitSquare(currentSquarePos, currentSquareDistance, nextSquare, nextSquares, visited)
+            }
+            if (currentSquarePos.second - 1 >= 0) {
+                val nextSquare = Pair(currentSquarePos.first, currentSquarePos.second - 1)
+                visitSquare(currentSquarePos, currentSquareDistance, nextSquare, nextSquares, visited)
+            }
+            if (currentSquarePos.second + 1 < this.first().size) {
+                val nextSquare = Pair(currentSquarePos.first, currentSquarePos.second + 1)
+                visitSquare(currentSquarePos, currentSquareDistance, nextSquare, nextSquares, visited)
+            }
         }
     }
 
+    val endPosition = findPosition('E').first()
+    return visited[endPosition.first][endPosition.second].second
+}
+
+private fun List<CharArray>.visitSquare(
+    currentSquarePos: Pair<Int, Int>,
+    currentSquareDistance: Int,
+    nextSquarePos: Pair<Int, Int>,
+    nextSquares: MutableList<Pair<Int, Int>>,
+    visited: List<MutableList<Pair<Boolean, Int>>>
+) {
+    if (atMostOneStepHigher(currentSquarePos, nextSquarePos)) {
+        nextSquares.add(nextSquarePos)
+        val nextSquare = visited[nextSquarePos.first][nextSquarePos.second]
+        val distanceToNextSquare = nextSquare.second
+        if (currentSquareDistance + 1 < distanceToNextSquare) {
+            visited[nextSquarePos.first][nextSquarePos.second] = Pair(nextSquare.first, currentSquareDistance + 1)
+        }
+    }
+}
+
+private fun List<CharArray>.atMostOneStepHigher(current: Pair<Int, Int>, next: Pair<Int, Int>): Boolean {
+    return if (this[current.first][current.second] == 'S') {
+        this[next.first][next.second] == 'a'
+    } else if (this[next.first][next.second] == 'E') {
+        true
+    } else {
+        this[next.first][next.second].minus(this[current.first][current.second]) <= 1
+    }
 }
