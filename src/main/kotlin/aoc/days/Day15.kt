6efe5b88ccf9f44d292027f4a98c72f2cdeee0be
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-15 12:54:35 +0100

    Day 15, part 2 WIP (heap error again 😭)

--- src/main/kotlin/aoc/days/Day15.kt
+++ src/main/kotlin/aoc/days/Day15.kt
@@ -8,8 +8,7 @@ private val INPUT_LINE = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at
 class Day15 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
-        val sensors = input
-            .map { SensorBeacon.of(it) }
+        val sensors = input.map { SensorBeacon.of(it) }
 
         return (-10_000_000..10_000_000).count { x ->
             sensors.any { it.withinRange(Pair(x, 2_000_000)) }
@@ -17,7 +16,13 @@ class Day15 : Day() {
     }
 
     override fun solvePart2(input: List<String>): Any {
-        return ""
+        val sensors = input.map { SensorBeacon.of(it) }
+
+        return sensors
+            .map { it.getPerimeterCoords() }
+            .flatten()
+            .also { println(it.count()) }
+            .first { coords -> sensors.none { it.withinRange(coords) } }
     }
 
 }
@@ -34,8 +39,19 @@ private data class SensorBeacon(val sensor: Pair<Int, Int>, val beacon: Pair<Int
         }
     }
 
-    fun withinRange(coordinate: Pair<Int, Int>): Boolean {
-        return manhattan(sensor, coordinate) <= range && coordinate != beacon
+    fun withinRange(coords: Pair<Int, Int>): Boolean {
+        return manhattan(sensor, coords) <= range && coords != beacon
+    }
+
+    fun getPerimeterCoords(): List<Pair<Int, Int>> {
+        return (IntRange(sensor.first - range - 1, sensor.first))
+            .mapIndexed { index, x -> listOf(Pair(x, sensor.second - index), Pair(x, sensor.second + index)) }
+            .flatten()
+            .plus(
+                ((sensor.first + range + 1) downTo sensor.first)
+                    .mapIndexed { index, x -> listOf(Pair(x, sensor.second - index), Pair(x, sensor.second + index)) }
+                    .flatten()
+            )
     }
 }
 
