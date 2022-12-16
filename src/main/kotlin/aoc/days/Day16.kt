febdfce41af3c82a568c5dccf7cf871c9724ab7a
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-16 21:42:35 +0100

    Day 16, part 1, finally!!

--- src/main/kotlin/aoc/days/Day16.kt
+++ src/main/kotlin/aoc/days/Day16.kt
@@ -1,7 +1,6 @@
 package aoc.days
 
 import aoc.Day
-import java.util.*
 
 private val INPUT_LINE = "Valve ([A-Z]+) has flow rate=(\\d+); tunnels? leads? to valves? ([A-Z ,]+)".toRegex()
 
@@ -10,27 +9,25 @@ class Day16 : Day() {
     override fun solvePart1(input: List<String>): Any {
         val valves = input.map { Valve.of(it) }
         val startValve = valves.first { it.name == "AA" }
-
-        val queue = PriorityQueue { v1: Valve, v2: Valve -> v2.pressure - v1.pressure }
-        val visitedValves = mutableSetOf(startValve)
-
-        queue.add(startValve)
-
-        while (queue.isNotEmpty()) {
-            val nextValve = queue.poll()
-            println("Visiting $nextValve")
-            val connectedValves = valves.getValves(nextValve.connectedValves)
-
-            connectedValves
-                .filter { it !in visitedValves }
-                .forEach {
-                    it.pressure = nextValve.pressure + it.rate
-                    visitedValves.add(it)
-                    queue.add(it)
-                }
+        // Create a valves map, so we could easily retrieve a Valve object based on its name
+        // Otherwise, if we reference Valve objects in Valve objects themselves,
+        // we get StackOverflow errors because of circular dependencies.
+        val valveMap = valves.fold(mutableMapOf<String, Valve>()) { map, valve ->
+            map.plus(Pair(valve.name, valve)).toMutableMap()
         }
 
-        return ""
+        // Pre-calculate the minimum distance between the start valve and any other valve with non-zero flow rate.
+        // Calculated using BFS.
+        val valvesWithFlowRate = valves.filter { it.rate > 0 }.plus(startValve)
+        valvesWithFlowRate.forEach { valve ->
+            valvesWithFlowRate.forEach { otherValve ->
+                valve.addDistanceToValve(otherValve.name, valves.calculateDistanceTo(valve, otherValve))
+            }
+        }
+
+        // Calculate every possible path from the start valve to the valves with non-zero flow rate
+        // and retrieve the maximum possible flow.
+        return startValve.getMaximumFlow(valveMap, 30, mutableSetOf(startValve.name))
     }
 
     override fun solvePart2(input: List<String>): Any {
@@ -39,13 +36,79 @@ class Day16 : Day() {
 
 }
 
-private data class Valve(val name: String, val rate: Int, val connectedValves: List<String>, var pressure: Int = 0) {
+private data class Valve(
+    val name: String,
+    val rate: Int,
+    val connectedValves: List<String>,
+    var distanceToOtherValves: MutableMap<String, Int> = mutableMapOf()
+) {
+    fun addDistanceToValve(valve: String, distance: Int) {
+        distanceToOtherValves[valve] = distance
+    }
+
+    fun getMaximumFlow(
+        valveMap: Map<String, Valve>,
+        minutesLeft: Int,
+        visited: MutableSet<String>,
+        currentRate: Int = 0
+    ): Int {
+        val unvisitedAndReachableValves = distanceToOtherValves
+            .filter { (valve, distance) -> valve !in visited && minutesLeft + 2 >= distance }
+        //                                                                   ^^^ -1 for turning on current valve
+        //                                                                   ^^^ -1 for turning on the next valve
+
+        if (unvisitedAndReachableValves.isNotEmpty()) {
+            return unvisitedAndReachableValves
+                .maxOf { (valve, distance) ->
+                    val newFlow = (currentRate + rate) * distance + currentRate
+                    //            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The flow rate while we're travelling to the next valve
+                    //                                              ^^^^^^^^^^^ Rate while we're turning on the valve
+
+                    val maxFlowPathAhead = valveMap[valve]!!.getMaximumFlow(
+                        valveMap,
+
+                        (minutesLeft - distance).minus(if (name == "AA") 0 else 1),
+                        //                                 ^^^^^^^^^^^^ No need to turn on the start valve
+
+                        visited.plus(valve).toMutableSet(),
+                        (currentRate + rate)
+                    )
+                    return@maxOf newFlow + maxFlowPathAhead
+                }
+        }
+
+        // All valves open or no time left to open any more valves  => open current valve and wait until time runs out
+        return ((minutesLeft - 1) * (currentRate + rate)) + currentRate
+        //     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The flow rate while we're waiting until the time runs out
+        //                                                  ^^^^^^^^^^^ Rate while we're turning on the valve
+    }
+
     companion object {
         fun of(line: String): Valve {
             val (name, rate, connectedValves) = INPUT_LINE.find(line)!!.destructured
             return Valve(name, rate.toInt(), connectedValves.split(", "))
         }
     }
+
+}
+
+private fun List<Valve>.calculateDistanceTo(source: Valve, destination: Valve): Int {
+    val nextValves = mutableListOf(Pair(source, 0))
+    val visitedValves = mutableSetOf(source)
+
+    while (nextValves.isNotEmpty()) {
+        val (nextValve, distance) = nextValves.removeFirst()
+        if (nextValve.name == destination.name) return distance
+
+        getValves(nextValve.connectedValves)
+            .filter { it !in visitedValves }
+            .forEach {
+                visitedValves.add(it)
+                nextValves.add(Pair(it, distance + 1))
+            }
+    }
+
+    return 0
 }
 
 private fun List<Valve>.getValves(connectedValves: List<String>): List<Valve> {
