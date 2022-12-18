bfd077bc2b27b49fb99d8ef83619af5a4fe12a9d
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-18 10:47:38 +0100

    Day 18, refactor for part 2

--- src/main/kotlin/aoc/days/Day18.kt
+++ src/main/kotlin/aoc/days/Day18.kt
@@ -6,11 +6,9 @@ import kotlin.math.absoluteValue
 class Day18 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
-        val cubes = input
-            .map { it.split(",") }
-            .map { (x, y, z) -> Triple(x.toInt(), y.toInt(), z.toInt()) }
+        val lavaDroplet = LavaDroplet.of(input)
 
-        return cubes.sumOf { it.countExposedSides(cubes) }
+        return lavaDroplet.countExposedSides()
     }
 
     override fun solvePart2(input: List<String>): Any {
@@ -19,18 +17,52 @@ class Day18 : Day() {
 
 }
 
-private fun Triple<Int, Int, Int>.countExposedSides(cubes: List<Triple<Int, Int, Int>>): Int {
-    return 6 - this.countAdjacentCubesX(cubes) - this.countAdjacentCubesY(cubes) - this.countAdjacentCubesZ(cubes)
-}
+private class LavaDroplet(val lavaDroplets: List<Triple<Int, Int, Int>>) {
+    val lavaDroplet: List<List<List<Char>>>
 
-private fun Triple<Int, Int, Int>.countAdjacentCubesX(cubes: List<Triple<Int, Int, Int>>): Int {
-    return cubes.count { it.first.minus(this.first).absoluteValue == 1 && it.second == this.second && it.third == this.third }
-}
+    companion object {
+        fun of(input: List<String>): LavaDroplet {
+            val lavaDroplets = input
+                .map { it.split(",") }
+                .map { (x, y, z) -> Triple(x.toInt(), y.toInt(), z.toInt()) }
+            return LavaDroplet(lavaDroplets)
+        }
+    }
 
-private fun Triple<Int, Int, Int>.countAdjacentCubesY(cubes: List<Triple<Int, Int, Int>>): Int {
-    return cubes.count { it.second.minus(this.second).absoluteValue == 1 && it.first == this.first && it.third == this.third }
-}
+    init {
+        val largestX = lavaDroplets.maxOf { it.first }
+        val largestY = lavaDroplets.maxOf { it.second }
+        val largestZ = lavaDroplets.maxOf { it.third }
 
-private fun Triple<Int, Int, Int>.countAdjacentCubesZ(cubes: List<Triple<Int, Int, Int>>): Int {
-    return cubes.count { it.third.minus(this.third).absoluteValue == 1 && it.first == this.first && it.second == this.second }
+        lavaDroplet = (0..largestX).map {
+            (0..largestY).map {
+                (0..largestZ).map {
+                    '.'
+                }.toMutableList()
+            }.toMutableList()
+        }.toMutableList()
+
+        lavaDroplets.forEach { lavaDroplet[it.first][it.second][it.third] = '#' }
+    }
+
+    fun countExposedSides(): Int {
+        return lavaDroplets.sumOf { it.countExposedSides() }
+    }
+
+    private fun Triple<Int, Int, Int>.countExposedSides(): Int {
+        return 6 - this.countAdjacentCubesX() - this.countAdjacentCubesY() - this.countAdjacentCubesZ()
+    }
+
+    private fun Triple<Int, Int, Int>.countAdjacentCubesX(): Int {
+        return lavaDroplets.count { it.first.minus(this.first).absoluteValue == 1 && it.second == this.second && it.third == this.third }
+    }
+
+    private fun Triple<Int, Int, Int>.countAdjacentCubesY(): Int {
+        return lavaDroplets.count { it.second.minus(this.second).absoluteValue == 1 && it.first == this.first && it.third == this.third }
+    }
+
+    private fun Triple<Int, Int, Int>.countAdjacentCubesZ(): Int {
+        return lavaDroplets.count { it.third.minus(this.third).absoluteValue == 1 && it.first == this.first && it.second == this.second }
+    }
+
 }
