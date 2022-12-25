fd058b3bc3d71435e379bcda02c32428b1cad57f
ZhongXiLu <zhongxilu@hotmail.com>
2022-12-25 13:19:37 +0100

    Day 25, part 1

---
+++ src/test/kotlin/aoc/days/Day25Test.kt
@@ -0,0 +1,30 @@
+package aoc.days
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class Day25Test {
+
+    private val day = Day25()
+    private val testInput = listOf(
+        "1=-0-2",
+        "12111",
+        "2=0=",
+        "21",
+        "2=01",
+        "111",
+        "20012",
+        "112",
+        "1=-1=",
+        "1-12",
+        "12",
+        "1=",
+        "122"
+    )
+
+    @Test
+    fun testPart1() {
+        assertEquals("2=-1=0", day.solvePart1(testInput))
+    }
+
+}
