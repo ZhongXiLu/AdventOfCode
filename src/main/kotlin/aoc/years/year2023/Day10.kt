cf235da38c54309eb321bb80ded9b820df761b4f
ZhongXiLu <zhongxilu@hotmail.com>
2023-12-10 10:58:09 +0100

    2023 Day 10, part 1

---
+++ src/main/kotlin/aoc/years/year2023/Day10.kt
@@ -0,0 +1,88 @@
+package aoc.years.year2023
+
+import aoc.Day
+
+@Year2023
+class Day10 : Day() {
+
+    override fun solvePart1(input: List<String>): Any {
+        return Map.of(input).getFarthestDistanceFromStart()
+    }
+
+    override fun solvePart2(input: List<String>): Any {
+        return 0
+    }
+
+}
+
+private data class Map(val map: List<CharArray>) {
+    companion object {
+        fun of(input: List<String>): Map {
+            return Map(input.map { it.toCharArray() })
+        }
+    }
+
+    // Dijkstra
+    fun getFarthestDistanceFromStart(): Int {
+        val visited = this.map.indices
+            .map {
+                this.map.first().indices
+                    .map { Pair(false, -1) }
+                    .toMutableList()
+            }
+
+        val startingPosition = getStartingPosition()
+        val next = mutableListOf(startingPosition)
+        visited[startingPosition.first][startingPosition.second] = Pair(false, 0)
+
+        while (next.isNotEmpty()) {
+            val current = next.removeFirst()
+
+            val (currentVisited, currentDistance) = visited[current.first][current.second]
+            if (!currentVisited) {
+                visited[current.first][current.second] = Pair(true, currentDistance)
+
+                this.map.getConnectedPipes(current)
+                    .filter { it.first in this.map.indices && it.second in this.map.first().indices }
+                    .forEach {
+                        next.add(it)
+                        val nextSquare = visited[it.first][it.second]
+                        val distanceToNextSquare = nextSquare.second
+                        if (currentDistance + 1 < distanceToNextSquare || distanceToNextSquare == -1) {
+                            visited[it.first][it.second] = Pair(false, currentDistance + 1)
+                        }
+                    }
+            }
+        }
+
+        return visited.maxOf { row -> row.maxOf { it.second } }
+    }
+
+    private fun getStartingPosition(): Pair<Int, Int> {
+        map.forEachIndexed { x, row ->
+            row.forEachIndexed { y, char ->
+                if (char == 'S') {
+                    return Pair(x, y)
+                }
+            }
+        }
+        return Pair(-1, -1)
+    }
+
+}
+
+private fun List<CharArray>.getConnectedPipes(pos: Pair<Int, Int>): List<Pair<Int, Int>> {
+    return when (val pipe = this[pos.first][pos.second]) {
+        'S' -> listOf(Pair(pos.first - 1, pos.second), Pair(pos.first, pos.second + 1))      // hard coded :)
+//        'S' -> listOf(Pair(pos.first, pos.second + 1), Pair(pos.first + 1, pos.second))
+
+        '|' -> listOf(Pair(pos.first - 1, pos.second), Pair(pos.first + 1, pos.second))
+        '-' -> listOf(Pair(pos.first, pos.second - 1), Pair(pos.first, pos.second + 1))
+        'L' -> listOf(Pair(pos.first - 1, pos.second), Pair(pos.first, pos.second + 1))
+        'J' -> listOf(Pair(pos.first - 1, pos.second), Pair(pos.first, pos.second - 1))
+        '7' -> listOf(Pair(pos.first, pos.second - 1), Pair(pos.first + 1, pos.second))
+        'F' -> listOf(Pair(pos.first, pos.second + 1), Pair(pos.first + 1, pos.second))
+        '.' -> listOf()
+        else -> throw Exception("Unknown character: $pipe")
+    }
+}
---
+++ src/test/kotlin/aoc/years/year2023/Day10Test.kt
@@ -0,0 +1,44 @@
+package aoc.years.year2023
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class Day10Test {
+
+    @Test
+    fun testPart1() {
+        val day = Day10()
+        assertEquals(
+            4, day.solvePart1(
+                listOf(
+                    ".....",
+                    ".S-7.",
+                    ".|.|.",
+                    ".L-J.",
+                    ".....",
+                )
+            )
+        )
+    }
+
+    @Test
+    fun testPart1_2() {
+        val day = Day10()
+        assertEquals(
+            8, day.solvePart1(
+                listOf(
+                    "7-F7-",
+                    ".FJ|7",
+                    "SJLL7",
+                    "|F--J",
+                    "LJ.LJ7-F7-",
+                    ".FJ|7",
+                    "SJLL7",
+                    "|F--J",
+                    "LJ.LJ",
+                )
+            )
+        )
+    }
+
+}
