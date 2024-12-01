0f75d6dfa026b92b291bfc6fb4c699d943b39c95
ZhongXiLu <zhongxilu@hotmail.com>
2024-12-01 10:27:57 +0100

    2024 Day 1, small refactor

--- src/main/kotlin/aoc/years/year2024/Day01.kt
+++ src/main/kotlin/aoc/years/year2024/Day01.kt
@@ -7,32 +7,30 @@ import kotlin.math.abs
 class Day01 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
-        val (locationLists, firstLocationsList, secondLocationsList) = getLocations(input)
+        val (firstLocationsList, secondLocationsList) = getLocations(input)
 
-        return locationLists.indices.sumOf {
-            val firstLocation = firstLocationsList[it]
-            val secondLocation = secondLocationsList[it]
-            return@sumOf abs(firstLocation - secondLocation)
+        return firstLocationsList.indices.sumOf {
+            return@sumOf abs(firstLocationsList[it] - secondLocationsList[it])
         }
     }
 
     override fun solvePart2(input: List<String>): Any {
-        val (locationLists, firstLocationsList, secondLocationsList) = getLocations(input)
+        val (firstLocationsList, secondLocationsList) = getLocations(input)
 
-        return locationLists.indices.sumOf {
+        return firstLocationsList.indices.sumOf {
             val firstLocation = firstLocationsList[it]
             val secondListOccurrences = secondLocationsList.count { location -> location == firstLocation }
             return@sumOf firstLocation * secondListOccurrences
         }
     }
 
-    private fun getLocations(input: List<String>): Triple<List<List<String>>, List<Int>, List<Int>> {
+    private fun getLocations(input: List<String>): Pair<List<Int>, List<Int>> {
         val locationLists = input.map { it.split("   ") }
 
         val firstLocationsList = locationLists.map { it[0].toInt() }.sorted()
         val secondLocationsList = locationLists.map { it[1].toInt() }.sorted()
 
-        return Triple(locationLists, firstLocationsList, secondLocationsList)
+        return Pair(firstLocationsList, secondLocationsList)
     }
 
 }
