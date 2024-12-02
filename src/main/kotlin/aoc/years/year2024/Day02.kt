a6df9c71c8a4869a2d0dced169a9070523cde8a7
ZhongXiLu <zhongxilu@hotmail.com>
2024-12-02 08:49:36 +0100

    2024 Day 2, refactoring

--- src/main/kotlin/aoc/years/year2024/Day02.kt
+++ src/main/kotlin/aoc/years/year2024/Day02.kt
@@ -13,7 +13,7 @@ class Day02 : Day() {
         return getReports(input).count { it.safeWithToleration() }
     }
 
-    private fun getReports(input: List<String>) = input.map { it.split(" ") }.map { it.map { level -> level.toInt() } }
+    private fun getReports(input: List<String>) = input.map { it.split(" ") }.map { it.map(String::toInt) }
 }
 
 private fun List<Int>.safe(): Boolean {
@@ -25,25 +25,21 @@ private fun List<Int>.safeWithToleration(): Boolean {
 }
 
 private fun List<Int>.allIncreasing(): Boolean {
-    return this.zipWithNext().all { it.first >= it.second + 1 && it.first <= it.second + 3 }
+    return this.zipWithNext().all { (first, second) -> first in (second + 1)..(second + 3) }
 }
 
 private fun List<Int>.allDecreasing(): Boolean {
-    return this.zipWithNext().all { it.first <= it.second - 1 && it.first >= it.second - 3 }
+    return this.zipWithNext().all { (first, second) -> first in (second - 3)..(second - 1) }
 }
 
 private fun List<Int>.allIncreasingWithToleration(): Boolean {
-    return this.indices.map { i ->
-        val list = this.toMutableList()
-        list.removeAt(i)
-        return@map list.allIncreasing()
-    }.any { it }
+    return this.indices.any { i ->
+        this.toMutableList().apply { removeAt(i) }.allIncreasing()
+    }
 }
 
 private fun List<Int>.allDecreasingWithToleration(): Boolean {
-    return this.indices.map { i ->
-        val list = this.toMutableList()
-        list.removeAt(i)
-        return@map list.allDecreasing()
-    }.any { it }
+    return this.indices.any { i ->
+        this.toMutableList().apply { removeAt(i) }.allDecreasing()
+    }
 }
