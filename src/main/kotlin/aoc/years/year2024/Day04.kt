f118dfd1176f7a146a7bd4527dd9e3c3f068032a
ZhongXiLu <zhongxilu@hotmail.com>
2024-12-04 13:05:26 +0100

    2024 Day 4, part 2

--- src/main/kotlin/aoc/years/year2024/Day04.kt
+++ src/main/kotlin/aoc/years/year2024/Day04.kt
@@ -6,12 +6,11 @@ import aoc.Day
 class Day04 : Day() {
 
     override fun solvePart1(input: List<String>): Any {
-        val wordSearch = WordSearch.of(input)
-        return wordSearch.countXmas()
+        return WordSearch.of(input).getXmasCount()
     }
 
     override fun solvePart2(input: List<String>): Any {
-        return input
+        return WordSearch.of(input).getXmasCrossCount()
     }
 
 }
@@ -24,53 +23,66 @@ private class WordSearch(val grid: List<List<Char>>) {
         }
     }
 
-    fun countXmas(): Int {
-        var count = 0
-
-        grid.indices.forEach { row ->
-            grid[row].indices.forEach { column ->
-                if (grid[row][column] == 'X') {
-                    count += countXmas(row, column)
-                }
+    fun getXmasCount(): Int {
+        return grid.indices.sumOf { row ->
+            grid[row].indices.sumOf { column ->
+                getXmasCount(row, column)
             }
         }
-
-        return count
     }
 
+    private fun getXmasCount(row: Int, column: Int): Int {
+        val directions = listOf(
+            listOf(Pair(0, 0), Pair(0, 1), Pair(0, 2), Pair(0, 3)),
+            listOf(Pair(0, 0), Pair(0, -1), Pair(0, -2), Pair(0, -3)),
+            listOf(Pair(0, 0), Pair(1, 0), Pair(2, 0), Pair(3, 0)),
+            listOf(Pair(0, 0), Pair(-1, 0), Pair(-2, 0), Pair(-3, 0)),
+            listOf(Pair(0, 0), Pair(1, 1), Pair(2, 2), Pair(3, 3)),
+            listOf(Pair(0, 0), Pair(-1, -1), Pair(-2, -2), Pair(-3, -3)),
+            listOf(Pair(0, 0), Pair(1, -1), Pair(2, -2), Pair(3, -3)),
+            listOf(Pair(0, 0), Pair(-1, 1), Pair(-2, 2), Pair(-3, 3))
+        )
 
-    private fun countXmas(row: Int, column: Int): Int {
-        var count = 0
-
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(0, 1), Pair(0, 2), Pair(0, 3)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(0, -1), Pair(0, -2), Pair(0, -3)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(1, 0), Pair(2, 0), Pair(3, 0)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(-1, 0), Pair(-2, 0), Pair(-3, 0)))
-
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(1, 1), Pair(2, 2), Pair(3, 3)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(-1, -1), Pair(-2, -2), Pair(-3, -3)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(1, -1), Pair(2, -2), Pair(3, -3)))
-        count += countXmasInAllDirections2(row, column, listOf(Pair(0, 0), Pair(-1, 1), Pair(-2, 2), Pair(-3, 3)))
-
-        return count
+        return directions.sumOf { isXmasInDirection(row, column, it) }
     }
 
-    private fun countXmasInAllDirections2(row: Int, column: Int, positions: List<Pair<Int, Int>>): Int {
-        var isXmas = 0
-        positions.forEachIndexed { index, position ->
-            if (index == 0 && row + position.first in grid.indices && column + position.second in grid.first().indices  && grid[row + position.first][column + position.second] == 'X') {
-                isXmas++
-            }
-            if (index == 1 && row + position.first in grid.indices && column + position.second in grid.first().indices  && grid[row + position.first][column + position.second] == 'M') {
-                isXmas++
-            }
-            if (index == 2 && row + position.first in grid.indices && column + position.second in grid.first().indices  && grid[row + position.first][column + position.second] == 'A') {
-                isXmas++
-            }
-            if (index == 3 && row + position.first in grid.indices && column + position.second in grid.first().indices  && grid[row + position.first][column + position.second] == 'S') {
-                isXmas++
+    private fun isXmasInDirection(row: Int, column: Int, positions: List<Pair<Int, Int>>): Int {
+        val xmasLetters = listOf('X', 'M', 'A', 'S')
+        val validXmasLetters = positions.withIndex().count { (index, position) ->
+            isLetter(row + position.first, column + position.second, xmasLetters[index])
+        }
+        return if (validXmasLetters == 4) 1 else 0
+    }
+
+    fun getXmasCrossCount(): Int {
+        return grid.indices.sumOf { row ->
+            grid[row].indices.sumOf { column ->
+                if (grid[row][column] == 'A') getXmasCrossCount(row, column) else 0
             }
         }
-        return if (isXmas == 4) 1 else 0
     }
+
+    private fun getXmasCrossCount(row: Int, column: Int): Int {
+        val positionsMS = listOf(
+            Pair(listOf(Pair(-1, -1), Pair(-1, 1)), listOf(Pair(1, -1), Pair(1, 1))),
+            Pair(listOf(Pair(-1, 1), Pair(1, 1)), listOf(Pair(-1, -1), Pair(1, -1))),
+            Pair(listOf(Pair(1, 1), Pair(1, -1)), listOf(Pair(-1, -1), Pair(-1, 1))),
+            Pair(listOf(Pair(1, -1), Pair(-1, -1)), listOf(Pair(-1, 1), Pair(1, 1)))
+        )
+
+        return positionsMS.sumOf { (positionsM, positionsS) ->
+            isXmasCross(row, column, positionsM, positionsS)
+        }
+    }
+
+    private fun isXmasCross(row: Int, column: Int, positionsM: List<Pair<Int, Int>>, positionsS: List<Pair<Int, Int>>): Int {
+        val positions = positionsM + positionsS
+        val validXmasLetters = positions.count { position ->
+            isLetter(row + position.first, column + position.second, if (position in positionsM) 'M' else 'S')
+        }
+        return if (validXmasLetters == 4) 1 else 0
+    }
+
+    private fun isLetter(x: Int, y: Int, letter: Char) = (x in grid.indices && y in grid.first().indices && grid[x][y] == letter)
+
 }
